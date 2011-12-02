c3cf9e8f36197d9e5bc5fd5110367f90629e8cad
Edward Hotchkiss <e@ingk.com>
2011-12-02 10:56:03 -0800

    removed files with .gitignore

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-shortUrl
-=====
 
-***Node.js Tiny URL generator with analytics***
-Based on https://github.com/edwardhotchkiss/short
+# ShortURL
 
-**Setup**
+### Node.js Tiny URL generator with analytics
+
+### Based on https://github.com/edwardhotchkiss/short
+
+### Setup
 
 Download files, cd to downloaded folder and run:
 
 ```bash
 $ npm install
 ```
-***Basic API Usage***
+### Basic API Usage
 
 Just add the domain you'd like to the end of localhost:8080/api
 
--- short.js
+++ short.js
@@ -1,54 +1,57 @@
-   var url = require('url'),
-  mongoose = require('mongoose'),
-   express = require('express'),
-     short = require('short'),
-       app = express.createServer(),
-      port = process.env.PORT || 8080;
 
-mongoose.connect("mongodb://localhost/short");
+var url = require('url');
+var express = require('express');
+var short = require('short');
+
+var app = express.createServer();
+var port = process.env.PORT || 8080;
+
+short.connect("mongodb://localhost/short");
 
 app.get('/api/*', function (req, res) {
-	if (req.url === '/favicon.ico') {
-		return;
-	}
-	var removeApi = req.url.slice(5),
-	    URL = removeApi;
-	short.gen(URL, function (error, shortURL) {
-		if (error) {console.error(error);
-		} else {
-			var URL = shortURL.URL,
-			    hash = shortURL.hash,
-				tiny_url = "http://127.0.0.1:" + port + "/" + hash;
-			console.log("URL is " + URL + " " + tiny_url);
-			res.end(tiny_url);
-        }
-	});
+  if (req.url === '/favicon.ico') {
+    return;
+  }
+  var removeApi = req.url.slice(5),
+      URL = removeApi;
+  short.gen(URL, function (error, shortURL) {
+    if (error) {
+      console.error(error);
+    } else {
+      var URL = shortURL.URL;
+      var hash = shortURL.hash;
+      var tiny_url = "http://127.0.0.1:" + port + "/" + hash;
+      console.log("URL is " + URL + " " + tiny_url);
+      res.end(tiny_url);
+    }
+  });
 });
 
 app.get('*', function (req, res) {
-	if (req.url === '/favicon.ico') {
-		return;
-	}
-	var hash = req.url.slice(1);
-	
-	short.get(hash, function (error, shortURLObject) {
-		if (error) {console.error(error);
-		} else {
-			if (shortURLObject) {
-				var URL = shortURLObject[0].URL;
-				res.writeHead(302, {
-					"Location" : URL
-				});
-				res.end();
-			} else {
-				res.writeHead(200, { "Content-Type" : "text/html" });
-				res.write("URL not found!");
-				res.end();
-			}
-		}
-	});
+  if (req.url === '/favicon.ico') {
+    return;
+  }
+  var hash = req.url.slice(1);
+  short.get(hash, function (error, shortURLObject) {
+    if (error) {console.error(error);
+    } else {
+      if (shortURLObject) {
+        var URL = shortURLObject[0].URL;
+        res.writeHead(302, {
+          "Location" : URL
+        });
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type" : "text/html" });
+        res.write("URL not found!");
+        res.end();
+      }
+    }
+  });
 });
 
 app.listen(port, function () {
-	console.log('Server running on port ' + port);
+  console.log('Server running on port ' + port);
 });
+
+/* EOF */
